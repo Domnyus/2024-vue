332f50b1d014f36c2901379809eda8ff9efa7fe7
Domnyus <domnyus@gmail.com>
2024-10-22 14:23:08 -0300

    2nd commit

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const app = Vue.createApp({
-    template: '<h1>Hello World </h1>'
+    data () {
+        return {
+            title: "Hello World",
+            author: "Domnyus"
+        }
+    }
 });
 
 app.mount("#root");
